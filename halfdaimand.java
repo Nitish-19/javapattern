0de572cdc7e6a6abacb2f1ff72485a4c6cf4c232
Nitish kumar <113499545+Nitish-19@users.noreply.github.com>
2024-05-16 16:11:29 +0530

    Add files via upload

---
+++ halfdaimand.java
@@ -0,0 +1,24 @@
+package javapattern;
+
+public class halfdaimand {
+
+	public static void main(String[] args) {
+		int i,j,k;
+		for(i=1; i<=7; i++)//rows
+		{
+			for(j=7; j>i; j--)//space
+			{
+				System.out.print(" ");
+              				
+			}
+			for(k=1; k<=i;k++)//coloms
+			{
+			System.out.print("* ");	
+			} 
+			System.out.print("\n");
+		}
+		
+	
+	}
+	}
+
---
+++ javapattern4.java
@@ -0,0 +1,22 @@
+package javapattern;
+
+public class javapattern4 {
+
+	public static void main(String[] args) {
+		int i,j,k;
+		for(i=5; i>=1; i--)//rows is loop ko ulta kiya he javapattern3 ko dekh ke
+		{
+		for(j=i;j<5; j++)//space
+		{
+			System.out.print(" ");
+		}
+		for(k=1; k<=i; k++)
+		{
+		System.out.print("*");
+			
+		}
+		System.out.print("\n");
+	}
+
+}
+}
